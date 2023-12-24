f1119dcdb874806337de76d4b0ef37e9ec689697
Rimkomatic <mahapatrarik123@gmail.com>
2023-12-25 01:01:23 +0530

    Made the scrapper file
    The scrapper is made but the loop functionality or the next link scrape(auto scrape) is not addded yet

--- scrapper.js
+++ scrapper.js
@@ -1,35 +1,84 @@
-const axios = require('axios')
-const cheerio = require('cheerio')
-const express = require('express')
-// const { index } = require('cheerio/lib/api/traversing')
+const puppeteer = require('puppeteer');
 
-const PORT = 3030
+async function getData(URL) {
+  const browser = await puppeteer.launch({ headless: 'new' }); // launch a headful browser for debugging
+  const page = await browser.newPage();
 
-const app = express()
+  console.log("Browser created");
 
-async function scrape(URL)
-{
-    await axios.get(URL)
-    .then( res => {
-        const data = cheerio.load(res.data)
+  try {
+    // Increase navigation timeout to 60 seconds
+    await page.goto(URL, { timeout: 60000 });
 
-        console.log(data('img'))
+    console.log("Went to the page");
 
-        // data('.attribute-value').each( (index,element) => {
-        //     console.log("ok")
-        //     console.log(`${index} --> ${data(element)}`)
-        // } )
-    } ).catch(e => console.log(e))
+    await page.waitForSelector('td', { timeout: 60000 }) //
+    await page.waitForSelector('.cell-num', { timeout: 60000 }) //
+    
+
+    console.log("Page fully loaded");
+
+    const pokemon = await page.evaluate(() => {
+      
+        console.log("Started scraping");
+
+        const td = Array.from(document.querySelectorAll('td'));    
+        const cellNum = Array.from(document.querySelectorAll('.cell-num'))   
+        const dexNo = td[0].firstChild.innerHTML
+        
+        const type = []
+        const typeEle = Array.from(td[1].childNodes)
+        typeEle.forEach( t => {
+            if(t.innerHTML) type.push(t.innerHTML)
+        })
+
+        const species = td[2].innerHTML
+        const height = td[3].innerText
+        const weight = td[4].innerText
+
+        const abilities =[]
+
+        const abilitiesEle = Array.from(td[5].childNodes)
+        
+        abilities.push(abilitiesEle[0].lastChild.innerText)
+        if(abilitiesEle.length>2) 
+        {
+          if(abilitiesEle[2].lastChild.innerText) abilities.push(abilitiesEle[2].lastChild.innerText)
+          else abilities.push(abilitiesEle[2].firstChild.innerText)
+          
+        }
+
+        if(abilitiesEle.length>4) abilities.push(abilitiesEle[4].firstChild.innerText)
+    
+        const stats = []
+
+        let pos = 0
+        while(pos<20)
+        {
+          stats.push(cellNum[pos].innerHTML)
+          pos=pos+3
+        }
+
+        const pokemon = new Object()
+        pokemon.dexNo = dexNo
+        pokemon.type = type
+        // pokemon.species = species
+        pokemon.height = height
+        pokemon.weight = weight
+        pokemon.abilities = abilities
+        pokemon.stats=stats
+    
+        return pokemon
+    });
+
+    console.log(pokemon);
+
+  } catch (error) {
+    console.error('Error during navigation:', error.message);
+  } finally {
+    
+  }
 }
 
-app.get( '/' , (req,res) => {
-    res.send()
-} )
-
-async function startServer(PORT)
-{
-    await scrape('https://www.pokemon.com/us/pokedex/bulbasaur')
-    app.listen(PORT)
-}
-
-startServer(PORT)
+getData('https://pokemondb.net/pokedex/pidgey');
+// getData('https://pokemondb.net/pokedex/beedrill');
