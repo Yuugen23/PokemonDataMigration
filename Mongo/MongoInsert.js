f8d77d1704f995250c458e416d7ffb5d4eed0368
Rimkomatic <mahapatrarik123@gmail.com>
2024-01-10 23:02:49 +0530

    Revert "feat: establish database connection and insert pokemon data into collection"
    This reverts commit c0a3b87c7b0861cde3616543fecb2d83fa3ce73d.

--- Mongo/MongoInsert.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Pokemon = require('./Schema/Pokemon.Mongo')
-
-async function pokeInsert(pokemon) {
-    console.log(pokemon);
-    await Pokemon.create(pokemon)
-}
-
-module.exports = pokeInsert
--- scrapperBrain.js
+++ scrapperBrain.js
@@ -1,7 +1,4 @@
-
-const pokemonScrapper = require('./Scrapper/pokemonScraper')
-const mongo = require('./Mongo/Mongo');
-const pokeInsert = require('./Mongo/MongoInsert')
+const pokemonScrapper = require('./pokemonScraper')
 
 // console.log(process.argv[2])
 
@@ -9,32 +6,31 @@ const URL = process.argv[2] || 'https://pokemondb.net/pokedex/pidgey'
 const LIMIT = Number(process.argv[3]) || 1
 
 
-
-async function fetchData(URL, LIMIT, isMega, currentMega) {
-  if (LIMIT > 0) {
-    const [pokemon, next, mega] = await pokemonScrapper.getPokemon(URL, isMega, currentMega)
+ 
+async function fetchData(URL,LIMIT , isMega , currentMega)
+{
+  if(LIMIT > 0)
+  {
+    const [pokemon,next, mega ]  =  await pokemonScrapper.getPokemon(URL,isMega,currentMega)
     console.log(`Data fetched for ${pokemon.name}`)
-    try {
-      await pokeInsert(pokemon)
-    } catch (e) { console.log("insertion failed", e.message); }
+
     // console.log(pokemon)
     // console.log(next)
     // console.log(mega)
 
-    if (!isMega && mega && mega.length > 1 && pokemon.name !== 'Pikachu' && pokemon.name !== 'Eevee') {
+    if( !isMega && mega && mega.length > 1 && pokemon.name !== 'Pikachu' && pokemon.name !== 'Eevee' )
+    {
       console.log("in the mega loop")
       let megaPos = 1
-      while (megaPos < mega.length) {
-        await fetchData(URL, 1, true, megaPos)
-        megaPos = megaPos + 1
+      while(megaPos < mega.length)
+      {
+        await fetchData(URL,1,true,megaPos)
+        megaPos=megaPos+1
       }
     }
 
-    await fetchData(next, LIMIT - 1, false, 0)
+    await fetchData(next , LIMIT - 1 , false , 0)
   }
 }
-async function start() {
-  await mongo.mongoConnect()
-  fetchData(URL, LIMIT, false, 0)
-}
-start()
+
+fetchData(URL,LIMIT , false, 0)
