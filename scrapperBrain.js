42f2d66b2e2f6338094fbbe51265d65a089e5a32
Rimkomatic <mahapatrarik123@gmail.com>
2024-01-10 23:09:49 +0530

    Revert accidental push of sensitive data in .env file

--- scrapperBrain.js
+++ scrapperBrain.js
@@ -1,4 +1,7 @@
-const pokemonScrapper = require('./pokemonScraper')
+
+const pokemonScrapper = require('./Scrapper/pokemonScraper')
+const mongo = require('./Mongo/Mongo');
+const pokeInsert = require('./Mongo/MongoInsert')
 
 // console.log(process.argv[2])
 
@@ -6,31 +9,32 @@ const URL = process.argv[2] || 'https://pokemondb.net/pokedex/pidgey'
 const LIMIT = Number(process.argv[3]) || 1
 
 
- 
-async function fetchData(URL,LIMIT , isMega , currentMega)
-{
-  if(LIMIT > 0)
-  {
-    const [pokemon,next, mega ]  =  await pokemonScrapper.getPokemon(URL,isMega,currentMega)
-    console.log(`Data fetched for ${pokemon.name}`)
 
+async function fetchData(URL, LIMIT, isMega, currentMega) {
+  if (LIMIT > 0) {
+    const [pokemon, next, mega] = await pokemonScrapper.getPokemon(URL, isMega, currentMega)
+    console.log(`Data fetched for ${pokemon.name}`)
+    try {
+      await pokeInsert(pokemon)
+    } catch (e) { console.log("insertion failed", e.message); }
     // console.log(pokemon)
     // console.log(next)
     // console.log(mega)
 
-    if( !isMega && mega && mega.length > 1 && pokemon.name !== 'Pikachu' && pokemon.name !== 'Eevee' )
-    {
+    if (!isMega && mega && mega.length > 1 && pokemon.name !== 'Pikachu' && pokemon.name !== 'Eevee') {
       console.log("in the mega loop")
       let megaPos = 1
-      while(megaPos < mega.length)
-      {
-        await fetchData(URL,1,true,megaPos)
-        megaPos=megaPos+1
+      while (megaPos < mega.length) {
+        await fetchData(URL, 1, true, megaPos)
+        megaPos = megaPos + 1
       }
     }
 
-    await fetchData(next , LIMIT - 1 , false , 0)
+    await fetchData(next, LIMIT - 1, false, 0)
   }
 }
-
-fetchData(URL,LIMIT , false, 0)
+async function start() {
+  await mongo.mongoConnect()
+  fetchData(URL, LIMIT, false, 0)
+}
+start()
